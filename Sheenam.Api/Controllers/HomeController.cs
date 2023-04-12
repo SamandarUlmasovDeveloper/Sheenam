8e42e301d731036c7729e9dec70ffba51e6f9ff6
Samandar <olmasovsamandar120@gmail.com>
2023-04-12 14:46:50 +0500

    INFRA:Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//===============================================
+//CopyRight(c) colation of Good-Hearted Engineers
+//Free to  Use  comfort and Peace
+//===============================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("hello Mario");
+    }
+}
