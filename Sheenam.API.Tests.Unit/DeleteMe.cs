e309bb0a9ed9c3d6699c29fbeee594158cd32f3d
Samandar <olmasovsamandar120@gmail.com>
2023-05-11 17:01:38 +0500

    INFRA:Initilaize Unit Test project

---
+++ Sheenam.API.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿//===============================================
+//CopyRight(c) colation of Good-Hearted Engineers
+//Free to  Use  comfort and Peace
+//===============================================
+using Xunit;
+
+namespace Sheenam.API.Tests.Unit
+{
+public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()=> Assert.True(true);
+    }
+}
